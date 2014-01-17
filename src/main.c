00813bb9f0089031bac78faaa5e2b88597bf831c
Axel Steiner <ast@treibsand.com>
2014-01-17 17:21:25 +0100

    more small changes for api switch

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 /* spmfilter-spamassassin - spmfilter Spamassassin Plugin
- * Copyright (C) 2009-2010 Axel Steiner and SpaceNet AG
+ * Copyright (C) 2009-2014 Axel Steiner and SpaceNet AG
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
